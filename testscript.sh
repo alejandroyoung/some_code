4be41db6f321789bec0b592c86ab51b2e74686fa
Alejandro Young <alejandro.young@rackspace.co.uk>
2021-05-27 10:54:31 +0100

    removed some lines

--- testscript.sh
+++ testscript.sh
@@ -3,6 +3,3 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "This was added"
-echo "another added ale"
-echo "another second line"
-echo "added another line"
